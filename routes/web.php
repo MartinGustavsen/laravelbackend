5e3ca1c4ebc6324c54ea0a63e710eec5b32f8cfd
MartinGustavsen <MartinGustavsen1994@gmail.com>
2020-10-20 21:11:48 +0200

    Added webpage, for test with Heroku

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/a', function () {
     return view('welcome');
 });
-Route::get('/test/', function () {
+Route::get('/test', function () {
     return view('welcome');
 });
